7c910b5b753cdee2be60b8359febc763103a4a1d
lebedys <adomas.lebedys@protonmail.com>
2021-03-22 11:33:06 +0000

    move file access to load_pdf function

--- extract.py
+++ extract.py
@@ -3,8 +3,8 @@ import pdftotext
 import re
 
 sections = { 
-    # this will eventually be auto-generated from the file itself
-    # but it's manual for now
+    # this will eventually be auto-generated from 
+    # the file itself but it's manual for now
     'contents': (0, 2),
     'changelog': (2, 5),
     'abbreviations': (5, 7),
@@ -14,63 +14,65 @@ sections = {
 def page_range(pages: tuple):
     return range(pages[0], pages[1])
 
-# todo get_pdf() that returns pdf object
+def load_pdf(file_path):
+    # todo - add some exception catching, handle reactions to non-pdf files
+    with open(file_path, 'rb') as f:
+        return pdftotext.PDF(f)
 
 def parse(file_path):
-    with open(file_path, 'rb') as f: # Load PDF
-        pdf = pdftotext.PDF(f)
+    pdf = load_pdf(file_path)
+    
+    # for page_number, page in enumerate(pdf):
+    #     if page_number in page_range(sections['contents']):
+    #         print(page_number)
+    #         # print(page)
         
-        # for page_number, page in enumerate(pdf):
-        #     if page_number in page_range(sections['contents']):
-        #         print(page_number)
-        #         # print(page)
+    #     if page_number >= sections['rules'][0]:
+    #         text = "\n\n".join(page).splitlines()
+    
+    pdfText = "\n\n".join(pdf).splitlines()
+
+    currentLevelOne = ""
+    
+    for line in pdfText:
+        footerMatch = re.match(r'Formula Student Rules 2020 +'
+                            'Version: (\d+\.?)* +'
+                            '\d+ of \d+', line)
+    
+        if footerMatch:
+            continue
+
+        headingUnderlines = ['=', '-']
+
+        # Match heading
+        mo = re.match(r'^(A|T|[CED]V|IN|S|D) +(\d+\.?)*', line)
+    
+        if mo:
+            headingLevel = len(mo.group().split('.'))
+
+            # Determine between top of page heading and level 1 headings
+            if headingLevel == 1:
+                if line != currentLevelOne:
+                    currentLevelOne = line;
+                else:
+                    continue;
             
-        #     if page_number >= sections['rules'][0]:
-        #         text = "\n\n".join(page).splitlines()
-        
-        pdfText = "\n\n".join(pdf).splitlines()
 
-        currentLevelOne = ""
-        
-        for line in pdfText:
-            footerMatch = re.match(r'Formula Student Rules 2020 +'
-                                'Version: (\d+\.?)* +'
-                                '\d+ of \d+', line)
-        
-            if footerMatch:
-                continue
+            # If we have a heading not a rule
+            if headingLevel < 3:
+                headingText = line[mo.end():].strip()
 
-            headingUnderlines = ['=', '-']
+                heading = mo.group() + ' - ' + headingText;
 
-            # Match heading
-            mo = re.match(r'^(A|T|[CED]V|IN|S|D) +(\d+\.?)*', line)
-        
-            if mo:
-                headingLevel = len(mo.group().split('.'))
+                # Add 3 for dash
+                underlineLength = len(headingText) + len(mo.group()) + 3
+                underlines = headingUnderlines[headingLevel-1]*underlineLength
 
-                # Determine between top of page heading and level 1 headings
-                if headingLevel == 1:
-                    if line != currentLevelOne:
-                        currentLevelOne = line;
-                    else:
-                        continue;
-                
+                print(heading)
+                print(underlines)
 
-                # If we have a heading not a rule
-                if headingLevel < 3:
-                    headingText = line[mo.end():].strip()
-
-                    heading = mo.group() + ' - ' + headingText;
-
-                    # Add 3 for dash
-                    underlineLength = len(headingText) + len(mo.group()) + 3
-                    underlines = headingUnderlines[headingLevel-1]*underlineLength
-
-                    print(heading)
-                    print(underlines)
-
-                # If we have a rule
-                if headingLevel == 3:
-                    print(line)
+            # If we have a rule
+            if headingLevel == 3:
+                print(line)
 
 parse('rules.pdf')
