9bb8fe5c3559232275ffceb37bab2a5f82e3c9cc
Max O'Brien <max@maxobrien.co.uk>
2021-03-22 15:33:27 +0000

    Added rule multiline formatting

--- extract.py
+++ extract.py
@@ -2,15 +2,31 @@ import pdftotext
 
 import re
 
+abbrPage = 6
+startPage = 8
+
 # Load PDF
 with open("rules.pdf", "rb") as f:
-    pdf = pdftotext.PDF(f)
-
-    pdfText = "\n\n".join(pdf).splitlines()
+    abbrText = ""
+    pdfText = ""
 
     currentLevelOne = ""
+    multilineRule = False
+    ruleText = ""
+    prevRuleText = ""
+    ruleGroup = ""
+
+    pdf = pdftotext.PDF(f)
+
+    for i in range(abbrPage - 1, startPage - 1):
+        abbrText += pdf[i]
+
+    for i in range(startPage - 1, len(pdf)):
+        pdfText += pdf[i]
+
+    # pdfText = "\n".join(pdf).splitlines()
     
-    for line in pdfText:
+    for line in pdfText.splitlines():
         footerMatch = re.match(r'Formula Student Rules 2020 +'
                                'Version: (\d+\.?)* +'
                                '\d+ of \d+', line)
@@ -22,8 +38,12 @@ with open("rules.pdf", "rb") as f:
 
         # Match heading
         mo = re.match(r'^(A|T|[CED]V|IN|S|D) +(\d+\.?)*', line)
-    
+
         if mo:
+            if multilineRule:
+                print('  ' + prevRuleText + "\n") # Print rule text
+                multilineRule = False
+
             headingLevel = len(mo.group().split('.'))
 
             # Determine between top of page heading and level 1 headings
@@ -32,7 +52,6 @@ with open("rules.pdf", "rb") as f:
                     currentLevelOne = line;
                 else:
                     continue;
-            
 
             # If we have a heading not a rule
             if headingLevel < 3:
@@ -44,9 +63,23 @@ with open("rules.pdf", "rb") as f:
                 underlineLength = len(headingText) + len(mo.group()) + 3
                 underlines = headingUnderlines[headingLevel-1]*underlineLength
 
+                print("\n")
                 print(heading)
                 print(underlines)
 
             # If we have a rule
             if headingLevel == 3:
-                print(line)
+                ruleGroup = mo.group()
+                ruleText = line[mo.end():].strip()
+
+                if prevRuleText != ruleText:
+                    prevRuleText = ruleText
+
+                    print(ruleGroup)
+
+        else:
+            # Line of a rule without a 'heading'
+            prevRuleText += ' ' + line.strip()
+            multilineRule = True
+
+    print('  ' + prevRuleText + "\n") # Print final rule
