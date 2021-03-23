e97bb41051651b221b3539bc86a0aff72a19e111
lebedys <adomas.lebedys@protonmail.com>
2021-03-23 11:22:56 +0000

    allow parsing of sections and individual pages; currently still pring the page contents

--- extract.py
+++ extract.py
@@ -1,15 +1,6 @@
 import pdftotext
-
 import re
-
-sections = { 
-    # this will eventually be auto-generated from 
-    # the file itself but it's manual for now
-    'contents': (0, 2),
-    'changelog': (2, 5),
-    'abbreviations': (5, 7),
-    'rules': (7, 133)
-}
+import os
 
 def page_range(pages: tuple):
     return range(pages[0], pages[1])
@@ -18,23 +9,14 @@ def load_pdf(file_path):
     # todo - add some exception catching, handle reactions to non-pdf files
     with open(file_path, 'rb') as f:
         return pdftotext.PDF(f)
-
-def parse(file_path):
-    pdf = load_pdf(file_path)
     
-    # for page_number, page in enumerate(pdf):
-    #     if page_number in page_range(sections['contents']):
-    #         print(page_number)
-    #         # print(page)
-        
-    #     if page_number >= sections['rules'][0]:
-    #         text = "\n\n".join(page).splitlines()
-    
-    pdfText = "\n\n".join(pdf).splitlines()
+def parse_page(page):
+    page_lines = page.splitlines()
+    page_lines_split = [line.split() for line in page_lines]
 
     currentLevelOne = ""
     
-    for line in pdfText:
+    for line in page_lines:
         footerMatch = re.match(r'Formula Student Rules 2020 +'
                             'Version: (\d+\.?)* +'
                             '\d+ of \d+', line)
@@ -74,5 +56,34 @@ def parse(file_path):
             # If we have a rule
             if headingLevel == 3:
                 print(line)
+    
+def parse_section(pdf: pdftotext.PDF, section_pages: tuple):
+    '''
+    pdf: the result of pdftotext.PDF(PDF_FILE)
+    section_pages: page range for section you want to extract, e.g. (0, 5) for pages 0-4
+    '''
+    
+    for page_number, page in enumerate(pdf):
+        if page_number in page_range(section_pages):
+            parse_page(page)
+        else:
+            pass # skip pages not in requested section
+    
+    return []
+
+def parse(file_path):
+    pdf = load_pdf(file_path)
+    
+    section_pages = { # todo - generate automatically
+        # this will eventually be auto-generated from 
+        # the file itself but it's done manually for now
+        'contents': (0, 2),
+        'changelog': (2, 5),
+        'abbreviations': (5, 7),
+        'rules': (7, len(pdf))
+    }
+    
+    rules = parse_section(pdf, section_pages['rules'])
 
 parse('rules.pdf')
+
