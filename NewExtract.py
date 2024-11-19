4c147b74d0c0c409216256f22c9322a3a061093b
Mr-SkinnyLegs <16matthewvo@gmail.com>
2024-11-19 18:16:00 +0000

    clean pages

--- NewExtract.py
+++ NewExtract.py
@@ -1,6 +1,7 @@
 from PyPDF2 import PdfReader
 import re
 from collections import OrderedDict
+import copy
 
 
 class PDFReader():
@@ -8,7 +9,9 @@ class PDFReader():
     def __init__(self, filepath) -> None:
 
         self.pagesDict = PDFReader.getPageDict(filepath)
-        self.cleanPages()
+
+        for pageNum in self.pagesDict.keys():
+            self.pagesDict[pageNum] = self.cleanPage(self.pagesDict[pageNum])
 
 
     @staticmethod
@@ -22,14 +25,32 @@ class PDFReader():
 
             pageData = pageDataObj.extract_text(0).split("\n")
 
-            pagesDict[page] = pageData
+            # Add page to dict of PageNum:PageData, but remove first 5 elements (it contains the footer from previous page)
+            pagesDict[page] = pageData[5:]
 
         return pagesDict
 
 
-    def cleanPages(self):
-        # for page in 
-        pass
+    def cleanPage(self, pageData):
+        tmpPage = []
+
+        i = 0
+        print(f"Len page:{len(pageData)-2}")
+        while i < len(pageData)-3:
+            firstStr = i
+            tmpStr = pageData[firstStr]
+            for stringPos in range(firstStr, len(pageData)-2):  # -2 because we do +1 in the loop
+                i = stringPos
+                if (pageData[stringPos+1][0].islower()):    #(j != self.numPages-1) and 
+                    tmpStr = tmpStr + pageData[stringPos+1]
+                else:
+                    i += 1
+                    break
+            
+            tmpPage.append(tmpStr)
+        
+        return tmpPage
+
 
 
 obj = PDFReader(filepath="rules-extraction/fsuk-2024-rules---v1-2.pdf")
